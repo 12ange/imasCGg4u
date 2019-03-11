82510207b3929bba29fbc87237fbe82bbcf7cea4
12ange <flair.t9@gmail.com>
2019-03-12 08:42:18 +0900

    ( thatring 設定変更前のバックアップ )
    どう変更するかは README.md 末尾に書いた

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
-﻿# Idol Master Cinderella Girls SVG Icon
+﻿# Idol Master Cinderella Girls Icons on SVG
 
-* SVGファイル形式での配布
-* 特徴
-	* 画像側に色指定が無いので、塗色を使うときに選択できる
-		* そのまま使えばテキストの文字色が塗色になるので、自ずとテキストと画像の色が統一される
-* ~~~filename.svg#identifier~~~ で個別参照
+内部的にはSVGを利用した、アイコン(類)
 
 ## thatring.js - Web Components 版・デレステのメニュー背景にあるあの輪っか
 
@@ -18,3 +14,60 @@
 ```
 
 でざっくり指定すれば、使える。詳しくは /thatring.htm に使用例があるのでそっちで。
+
+## ひょっとして
+
+属性アイコンも Custom Element 風に実装すればいいのでは。
+
+未定義のタグは、とりあえず span タグのような見た目になる？  
+例えば `<undefined-tag>content</undefined-tag>` とした場合、content と表示される
+
+```html
+<imascgss-typeicon
+	data-idoltype="cute"
+	data-iconstyle="major"
+>(Cu)<!-- 代替文字列 --></imascgss-typeicon>
+```
+とすればフォールバックもいける。たぶん。
+
+```css
+imascgss-typeicon :not(:defined){ opacity:0; }
+```
+で、未定義状態で表示された場合に隠してもよい。
+
+> ### 覚え書き: Custom Element の定義を含む Javascript の実行タイミング
+>
+> ブラウザーは Google Chrome 72, thatringを使って実験した結果
+>
+> 1. HTML読込・解析完了
+> 1. deferで読み込んだJSを実行開始
+> 1. `window.customElements.define( dashed-tag-name, class )` に当たった
+> 1. 対応するクラスのコンストラクタを「使われた回数」実行
+> 1. JS実行完了、`DOMContentLoaded` イベント発火
+>
+> という流れでした。  
+> ページ内scriptなどで出現前に置くと何故か反映されなかったので、HTML解析完了してないと上手く働かないのかしら。
+>
+> https://developers.google.com/web/fundamentals/web-components/customelements?hl=ja
+>
+> HTML解析完了時点で、後の CustEle は (＝ CustEle として使える名前がついた要素は・一時的に) `HTMLElement` 型とみなされる……だからまっさらから CustEle を作るときは `extend HTMLElement` でないとなんだね。
+
+DOMツリーを動的に生成するなら結局JSなので、
+* JSが直接触るビルダークラス
+* ↑を継承するなり保持するなりした CustEle 専用無名クラス (define関数の第2引数に直書き)
+の2本立てにして、HTMLを手打ちしてJSノータッチの静的ページは CustEle で簡単に使えるようにすればいいかな。
+
+----
+
+1. attributeChangedCallback()をうまいこと使いたい
+1. けれど data-pattern と data-hint* が不可分な存在である
+1. 💡 data-pattern を JSON で要求すればいいじゃん 💡 そしてhint配列をpattern配列に埋め込めばいいじゃん
+1. 💭 data-widths と data-pattern はともに(本来は) doublet なので、JSON 形式で doublet 単位で要求する風にしても良さげ
+1. 💥 data-widths と data-pattern もそれ自体が doublet に出来るじゃん。つまり `[ [ <半径>,<幅>,<内側の形状=[]>,<外側の形状=[]> ], ...]` とまとめてしまって、それを監視する要素にするのです
+
+てことで
+
+* 今まで通りの `data-colors`
+* 形状情報を集約した `data-json`
+
+の 2 要素を監視すればいいってことですね。
--- thatring.js
+++ thatring.js
@@ -16,150 +16,150 @@
 //		+ --length-size   ... 画像の大きさ
 //		+ --time-per-turn ... 1回転当たりの時間
 //========================================================================
+class imascgssThatring extends HTMLElement{
 
-class imascgssThatring extends HTMLElement{ constructor(){
-//コンストラクタのみのクラス：内部構造は後から変えられない
+	//--初期値用の定数--
+	static get defaultColorinstr(){ return ["#d995ac","#f59fa0","#ffb195","#ffc887","#f9e08a","#d9d784","#8dd3ad","#71c5be","#71b1c7","#7e9dc3","#9c95c1","#b48fb5"] }
+		// PCCS->RGB http://www.wsj21.net/ghp/ghp0c_03.html 使うのは、PCCS light 色相環(12色)
+	static get defaultStripwidths(){ return [150,1,7,2,4,24,2,3,6,1] }
+	static get defaultStrippattern(){return [  0,0,0,0,0, 0,0,0,0,0] }
 
-	super(); //いの一番に親クラスのコンストラクタを呼んであげよう
+	//--サブルーチン--
+	newTagSVG = tag=>document.createElementNS("http://www.w3.org/2000/svg",tag);
+	//csv2arr* : CSVを(Str:文字列|Int:整数)配列に
+	csv2arrStr = (_str,_fallback=[])=> _str ? _str.split(",") : _fallback;
+	csv2arrInt = (_str,_fallback=[])=> _str ? _str.split(",").map(s=>s|0) : _fallback;
 
-	const NEW_SVGTAG = tag => document.createElementNS("http://www.w3.org/2000/svg",tag);
 	//積分(配列)
-	const integral = a=>{
+	integral = a=>{
 		let b=[], s=0;
 		for(let i in a){ b[i] = s += a[i]; }
 		return b;
 	};
 	//標準化(配列,倍率,下駄)
-	const normalize = (a,m=1,b=0)=>{
+	normalize = (a,m=1,b=0)=>{
 		const x = Math.max(...a);
 		return a.map( v=>m*v/x+b );
 	};
 	//円に内接する正多角形の辺の長さ
-	const edgelen_regpoly = num_angle=>{
+	edgelen_regpoly = num_angle=>{
 		let w = 2*Math.PI/num_angle;
 		let dx = Math.sin(w);
 		let dy = 1-Math.cos(w);
 		return Math.sqrt(dx*dx+dy*dy);
 	};
-	//CSVを文字列配列に
-	const csv2arrStr = (_str,_fallback=[])=>_str ? _str.split(",") : _fallback;
-	//CSVを数値配列に
-	const csv2arrInt = (_str,_fallback=[])=>_str ? _str.split(",").map(s=>s|0) : _fallback;
 
-	//初期値
-	const defVal_colorinstr = ["#d995ac","#f59fa0","#ffb195","#ffc887","#f9e08a","#d9d784","#8dd3ad","#71c5be","#71b1c7","#7e9dc3","#9c95c1","#b48fb5"];
-	// PCCS->RGB http://www.wsj21.net/ghp/ghp0c_03.html 使うのは、PCCS light 色相環(12色)
-	const defVal_stripwidths  = [150,1,7,2,4,24,2,3,6,1];
-	const defVal_strippattern = [  0,0,0,0,0, 0,0,0,0,0];
+	constructor(){ super();
+		//いの一番↑に親クラスのコンストラクタを必ず呼ぶ
 
-	//dataSet読み取り
-	const datasets = this.dataset;
-	let colorinstr = csv2arrStr( datasets["colors"], defVal_colorinstr );
-	let stripwidths = csv2arrInt( datasets["widths"], defVal_stripwidths );
-	let strippattern = csv2arrInt( datasets["pattern"], defVal_strippattern );
-	let striphints = (_maxhints=>{
-		let retarr = [];
-		for(let idx = 1 ; idx <= _maxhints ; idx++){
-			retarr.push( csv2arrInt(datasets[`hint${idx}`]) );
-		}
-		return retarr;
-	})( Math.max(...strippattern) );
-
-	//stripwidthsは積分して0～1に標準化、striphintsは積分して0～2piに標準化する。
-	let stripwidths_i = normalize( integral(stripwidths) );
-	let striphints_i = [];
-	for(let h of striphints){ striphints_i.push( normalize( integral(h), 2*Math.PI ) ); }
-	//円環の中央部半径
-	const rad_midstrip = stripwidths_i[0]+(1-stripwidths_i[0])/2;
-	//色付け円の中心同士の距離(＝指定する半径)
-	const gap_colorcircle = rad_midstrip * edgelen_regpoly(colorinstr.length);
-	//「切り抜く形状」を表す文字列(<path>の d プロパティ)を生成する
-	let dstr = "";
-	for( let i = 0 ; i < stripwidths_i.length ; ++i ){
-		let r = stripwidths_i[i]; //対象半径
-		dstr += `M${r},0`;
-		if( strippattern[i] === 0 ){
-			dstr += `A${r},${r} 0 0 ${i&1} -${r},0`;
-			dstr += `A${r},${r} 0 0 ${i&1} ${r},0z`;
-		}
-		else {
-			let q = stripwidths_i[i-1]; //直前半径
-			let ah = striphints_i[strippattern[i]-1]; //配列：切り替える角度
-			for( let j = 0 ; j < ah.length ; ++j ){
-				let s = (j&1?q:r), t = (j&1?r:q); //s=主、t=従 //反時計回りはY方向を反転
-				let mx = Math.cos(ah[j]), my = Math.sin(i&1?ah[j]:-ah[j]); //
-				dstr += `A${s},${s} 0 0 ${i&1} ${s*mx},${s*my}`; //円弧
-				dstr += (j+1 === ah.length ? "z" : `L${t*mx},${t*my}`); //直線
+		//dataSet読み取り
+		const datasets = this.dataset;
+		let colorinstr = this.csv2arrStr( datasets["colors"], imascgssThatring.defaultColorinstr );
+		let stripwidths = this.csv2arrInt( datasets["widths"], imascgssThatring.defaultStripwidths );
+		let strippattern = this.csv2arrInt( datasets["pattern"], imascgssThatring.defaultStrippattern );
+		let striphints = (_maxhints=>{
+			let retarr = [];
+			for(let idx = 1 ; idx <= _maxhints ; idx++){
+				retarr.push( this.csv2arrInt(datasets[`hint${idx}`]) );
 			}
+			return retarr;
+		})( Math.max(...strippattern) );
+
+		//stripwidthsは積分して0～1に標準化、striphintsは積分して0～2piに標準化する。
+		let stripwidths_i = this.normalize( this.integral(stripwidths) );
+		let striphints_i = [];
+		for(let h of striphints){ striphints_i.push( this.normalize( this.integral(h), 2*Math.PI ) ); }
+		//円環の中央部半径
+		const rad_midstrip = stripwidths_i[0]+(1-stripwidths_i[0])/2;
+		//色付け円の中心同士の距離(＝指定する半径)
+		const gap_colorcircle = rad_midstrip * this.edgelen_regpoly(colorinstr.length);
+		//「切り抜く形状」を表す文字列(<path>の d プロパティ)を生成する
+		let dstr = "";
+		for( let i = 0 ; i < stripwidths_i.length ; ++i ){
+			let r = stripwidths_i[i]; //対象半径
+			dstr += `M${r},0`;
+			if( strippattern[i] === 0 ){
+				dstr += `A${r},${r} 0 0 ${i&1} -${r},0`;
+				dstr += `A${r},${r} 0 0 ${i&1} ${r},0z`;
+			}
+			else {
+				let q = stripwidths_i[i-1]; //直前半径
+				let ah = striphints_i[strippattern[i]-1]; //配列：切り替える角度
+				for( let j = 0 ; j < ah.length ; ++j ){
+					let s = (j&1?q:r), t = (j&1?r:q); //s=主、t=従 //反時計回りはY方向を反転
+					let mx = Math.cos(ah[j]), my = Math.sin(i&1?ah[j]:-ah[j]); //
+					dstr += `A${s},${s} 0 0 ${i&1} ${s*mx},${s*my}`; //円弧
+					dstr += (j+1 === ah.length ? "z" : `L${t*mx},${t*my}`); //直線
+				}
+			}
+		}	// A rx,ry 楕円の傾き 大回り(1|0) 時計回り(1|0) x,y
+
+		//クリップパス準備
+		const clip = this.newTagSVG("clipPath");
+		clip.setAttribute("clip-rule","evenodd");
+		clip.appendChild(
+			//<path>を生成して直接append
+			( p =>{
+				p.setAttribute("d",dstr);
+				return p;
+			})( this.newTagSVG("path") )
+		);
+		//<defs> HTML化した時に(多少)わかりやすくなる(はず)
+		const defz = this.newTagSVG("defs");
+		defz.appendChild(clip);
+		//<g>
+		const grp = this.newTagSVG("g");
+		grp.id = "thatring";
+		clip.id = "path"+grp.id; //<g>のidを加工して<clipPath>のidを決める(ことで重複させない)
+		grp.setAttribute("clip-path",`url(#${clip.id})`);
+
+		//「色配置」円形に配置
+		for( let index = 0 ; index < colorinstr.length ; ++index ){
+			//グラデーション設定
+			let raGdt = this.newTagSVG("radialGradient");
+			raGdt.id = `filler${index}`;
+			let s1 = this.newTagSVG("stop");
+			s1.setAttribute("offset","0");
+			s1.setAttribute("stop-color",colorinstr[index]);
+			s1.setAttribute("stop-opacity","1");
+			let s2 = this.newTagSVG("stop");
+			s2.setAttribute("offset","1");
+			s2.setAttribute("stop-color",colorinstr[index]);
+			s2.setAttribute("stop-opacity","0");
+			raGdt.appendChild(s1);
+			raGdt.appendChild(s2);
+			defz.appendChild(raGdt);
+			//上を利用した円の描画
+			let c1 = this.newTagSVG("circle");
+			c1.cx.baseVal.value = rad_midstrip*Math.sin(2*Math.PI*index/colorinstr.length);
+			c1.cy.baseVal.value = rad_midstrip*Math.cos(2*Math.PI*index/colorinstr.length);
+			c1.r.baseVal.value = gap_colorcircle;
+			c1.style.fill = `url(#${raGdt.id})`;
+			grp.appendChild(c1);
 		}
-	}	// A rx,ry 楕円の傾き 大回り(1|0) 時計回り(1|0) x,y
 
-	//クリップパス準備
-	const clip = NEW_SVGTAG("clipPath");
-	clip.setAttribute("clip-rule","evenodd");
-	clip.appendChild(
-		//<path>を生成して直接append
-		( p =>{
-			p.setAttribute("d",dstr);
-			return p;
-		})( NEW_SVGTAG("path") )
-	);
-	//<defs> HTML化した時に(多少)わかりやすくなる(はず)
-	const defz = NEW_SVGTAG("defs");
-	defz.appendChild(clip);
-	//<g>
-	const grp = NEW_SVGTAG("g");
-	grp.id = "thatring";
-	clip.id = "path"+grp.id; //<g>のidを加工して<clipPath>のidを決める(ことで重複させない)
-	grp.setAttribute("clip-path",`url(#${clip.id})`);
+		//<style> 専用 CSS Style
+		let style = document.createElement("style");
+		style.textContent =`
+		@keyframes oneturn {
+			0% {transform:rotate(0turn)}
+			100% {transform:rotate(1turn)}
+		}
+		svg{
+			animation: oneturn var(--time-per-turn,10s) linear infinite;
+			height: var(--length-size,50vmin);
+			width: var(--length-size,50vmin);
+		}`;
 
-	//「色配置」円形に配置
-	for( let index = 0 ; index < colorinstr.length ; ++index ){
-		//グラデーション設定
-		let raGdt = NEW_SVGTAG("radialGradient");
-		raGdt.id = `filler${index}`;
-		let s1 = NEW_SVGTAG("stop");
-		s1.setAttribute("offset","0");
-		s1.setAttribute("stop-color",colorinstr[index]);
-		s1.setAttribute("stop-opacity","1");
-		let s2 = NEW_SVGTAG("stop");
-		s2.setAttribute("offset","1");
-		s2.setAttribute("stop-color",colorinstr[index]);
-		s2.setAttribute("stop-opacity","0");
-		raGdt.appendChild(s1);
-		raGdt.appendChild(s2);
-		defz.appendChild(raGdt);
-		//上を利用した円の描画
-		let c1 = NEW_SVGTAG("circle");
-		c1.cx.baseVal.value = rad_midstrip*Math.sin(2*Math.PI*index/colorinstr.length);
-		c1.cy.baseVal.value = rad_midstrip*Math.cos(2*Math.PI*index/colorinstr.length);
-		c1.r.baseVal.value = gap_colorcircle;
-		c1.style.fill = `url(#${raGdt.id})`;
-		grp.appendChild(c1);
+		//<svg> SVGを構成・接続する
+		const rootsvg = this.newTagSVG("svg");
+		rootsvg.setAttribute("viewBox", "-1 -1 2 2");
+		rootsvg.appendChild(defz);
+		rootsvg.appendChild(grp);
+		//Shadow Root を作って、そこにぶら下げる
+		let shdwroot = this.attachShadow({mode:"open"})
+		shdwroot.appendChild(style);
+		shdwroot.appendChild(rootsvg);
 	}
-
-	//<style> 専用 CSS Style
-	let style = document.createElement("style");
-	style.textContent =`
-	@keyframes oneturn {
-		0% {transform:rotate(0turn)}
-		100% {transform:rotate(1turn)}
-	}
-	svg{
-		animation: oneturn var(--time-per-turn,10s) linear infinite;
-		height: var(--length-size,50vmin);
-		width: var(--length-size,50vmin);
-	}`;
-
-	//<svg> SVGを構成・接続する
-	const rootsvg = NEW_SVGTAG("svg");
-	rootsvg.setAttribute("viewBox", "-1 -1 2 2");
-	rootsvg.appendChild(defz);
-	rootsvg.appendChild(grp);
-	//Shadow DOM の根を作って、そこにぶら下げる
-	let shdwroot = this.attachShadow({mode:"open"})
-	shdwroot.appendChild(style);
-	shdwroot.appendChild(rootsvg);
-
-}} //end_of_class imascgssThatring
+} //end_of_class imascgssThatring
 window.customElements.define('imascgss-thatring',imascgssThatring);
