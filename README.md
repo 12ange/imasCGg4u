e84feba4dc61054e54b1097cb174d1e4e5769bd8
12ange <flair.t9@gmail.com>
2019-03-14 16:35:47 +0900

    thatring 更新
    * 形状指定の属性を1つにまとめた
    * 生成後の属性変更に対応

--- README.md
+++ README.md
@@ -7,13 +7,14 @@
 ```html
 <imascgss-thatring
 	data-colors="省略可能"
-	data-widths="円周"
-	data-pattern="JSON"
-	data-hintN="JSON"
+	data-json="JSON"
 ></imascgss-thatring>
 ```
 
 でざっくり指定すれば、使える。詳しくは /thatring.htm に使用例があるのでそっちで。
+
+**thatring 積み残し : 回る向き、時計回りに限らない。**
+
 
 ## ひょっとして
 
@@ -56,18 +57,3 @@ DOMツリーを動的に生成するなら結局JSなので、
 * JSが直接触るビルダークラス
 * ↑を継承するなり保持するなりした CustEle 専用無名クラス (define関数の第2引数に直書き)
 の2本立てにして、HTMLを手打ちしてJSノータッチの静的ページは CustEle で簡単に使えるようにすればいいかな。
-
-----
-
-1. attributeChangedCallback()をうまいこと使いたい
-1. けれど data-pattern と data-hint* が不可分な存在である
-1. 💡 data-pattern を JSON で要求すればいいじゃん 💡 そしてhint配列をpattern配列に埋め込めばいいじゃん
-1. 💭 data-widths と data-pattern はともに(本来は) doublet なので、JSON 形式で doublet 単位で要求する風にしても良さげ
-1. 💥 data-widths と data-pattern もそれ自体が doublet に出来るじゃん。つまり `[ [ <半径>,<幅>,<内側の形状=[]>,<外側の形状=[]> ], ...]` とまとめてしまって、それを監視する要素にするのです
-
-てことで
-
-* 今まで通りの `data-colors`
-* 形状情報を集約した `data-json`
-
-の 2 要素を監視すればいいってことですね。
--- thatring.js
+++ thatring.js
@@ -6,12 +6,8 @@
 //
 //	+ data-なんたら属性。CSV(カンマ区切り表現)で表す
 //		+ data-colors ... 設定色を#rrggbb表現で記す。
-//			PCCS->RGB http://www.wsj21.net/ghp/ghp0c_03.html 使うのは、PCCS light 色相環(12色)
-//		+ data-widths  ... 塗る塗らないを切り替える幅。
-//			[index&0]～[&1]間が塗られる。必ず偶数個にしてね。
-//		+ data-pattern ... 0=円 1以上=直前径とジグザグ(data-hintN参照)
-//		+ data-hintN   ... data-patternで非0にした値をどうジグザグさせるか。
-//			[index&0]外径[&1]内径。N はdata-patternで指定した数字に対応
+//		+ data-json  ... 形状を指定するJSON表記。二重配列で……
+//			+ [ [ <半径>,<幅>,<内側の形状=[]>,<外側の形状=[]> ], ...]
 //	+ CSSカスタムプロパティ(--*:*)による設定
 //		+ --length-size   ... 画像の大きさ
 //		+ --time-per-turn ... 1回転当たりの時間
@@ -19,16 +15,23 @@
 class imascgssThatring extends HTMLElement{
 
 	//--初期値用の定数--
-	static get defaultColorinstr(){ return ["#d995ac","#f59fa0","#ffb195","#ffc887","#f9e08a","#d9d784","#8dd3ad","#71c5be","#71b1c7","#7e9dc3","#9c95c1","#b48fb5"] }
+	static get defaultColors(){ return ["#d995ac","#f59fa0","#ffb195","#ffc887","#f9e08a","#d9d784","#8dd3ad","#71c5be","#71b1c7","#7e9dc3","#9c95c1","#b48fb5"] }
 		// PCCS->RGB http://www.wsj21.net/ghp/ghp0c_03.html 使うのは、PCCS light 色相環(12色)
-	static get defaultStripwidths(){ return [150,1,7,2,4,24,2,3,6,1] }
-	static get defaultStrippattern(){return [  0,0,0,0,0, 0,0,0,0,0] }
+	static get defaultStripJson(){ return [[150,1],[6,4],[4,20],[4,4],[6,1]] }
 
 	//--サブルーチン--
 	newTagSVG = tag=>document.createElementNS("http://www.w3.org/2000/svg",tag);
+	getTagsSVG = (tag,root=this.shadowRoot)=>root.getElementsByTagNameNS("http://www.w3.org/2000/svg",tag);
 	//csv2arr* : CSVを(Str:文字列|Int:整数)配列に
 	csv2arrStr = (_str,_fallback=[])=> _str ? _str.split(",") : _fallback;
 	csv2arrInt = (_str,_fallback=[])=> _str ? _str.split(",").map(s=>s|0) : _fallback;
+
+	//何かを負でない整数に変換
+	toIntZeroPlus = _any=>Math.max(_any|0,0);
+	//何かを負でない整数の配列に変換
+	toArrayIntZeroPlus = _any=>Array.isArray(_any)?_any.map(o=>Math.max(o|0,0)):[];
+	//外径を1とした場合の内径から帯の中央径を導出
+	calcMidstrip = _r=>_r+(1-_r)/2;
 
 	//積分(配列)
 	integral = a=>{
@@ -52,91 +55,24 @@ class imascgssThatring extends HTMLElement{
 	constructor(){ super();
 		//いの一番↑に親クラスのコンストラクタを必ず呼ぶ
 
-		//dataSet読み取り
-		const datasets = this.dataset;
-		let colorinstr = this.csv2arrStr( datasets["colors"], imascgssThatring.defaultColorinstr );
-		let stripwidths = this.csv2arrInt( datasets["widths"], imascgssThatring.defaultStripwidths );
-		let strippattern = this.csv2arrInt( datasets["pattern"], imascgssThatring.defaultStrippattern );
-		let striphints = (_maxhints=>{
-			let retarr = [];
-			for(let idx = 1 ; idx <= _maxhints ; idx++){
-				retarr.push( this.csv2arrInt(datasets[`hint${idx}`]) );
-			}
-			return retarr;
-		})( Math.max(...strippattern) );
-
-		//stripwidthsは積分して0～1に標準化、striphintsは積分して0～2piに標準化する。
-		let stripwidths_i = this.normalize( this.integral(stripwidths) );
-		let striphints_i = [];
-		for(let h of striphints){ striphints_i.push( this.normalize( this.integral(h), 2*Math.PI ) ); }
-		//円環の中央部半径
-		const rad_midstrip = stripwidths_i[0]+(1-stripwidths_i[0])/2;
-		//色付け円の中心同士の距離(＝指定する半径)
-		const gap_colorcircle = rad_midstrip * this.edgelen_regpoly(colorinstr.length);
-		//「切り抜く形状」を表す文字列(<path>の d プロパティ)を生成する
-		let dstr = "";
-		for( let i = 0 ; i < stripwidths_i.length ; ++i ){
-			let r = stripwidths_i[i]; //対象半径
-			dstr += `M${r},0`;
-			if( strippattern[i] === 0 ){
-				dstr += `A${r},${r} 0 0 ${i&1} -${r},0`;
-				dstr += `A${r},${r} 0 0 ${i&1} ${r},0z`;
-			}
-			else {
-				let q = stripwidths_i[i-1]; //直前半径
-				let ah = striphints_i[strippattern[i]-1]; //配列：切り替える角度
-				for( let j = 0 ; j < ah.length ; ++j ){
-					let s = (j&1?q:r), t = (j&1?r:q); //s=主、t=従 //反時計回りはY方向を反転
-					let mx = Math.cos(ah[j]), my = Math.sin(i&1?ah[j]:-ah[j]); //
-					dstr += `A${s},${s} 0 0 ${i&1} ${s*mx},${s*my}`; //円弧
-					dstr += (j+1 === ah.length ? "z" : `L${t*mx},${t*my}`); //直線
-				}
-			}
-		}	// A rx,ry 楕円の傾き 大回り(1|0) 時計回り(1|0) x,y
-
-		//クリップパス準備
+		//SVG各種要素準備
+		const svgroot = this.newTagSVG("svg");
+		this.elmDefs = this.newTagSVG("defs"); //<clipPath>,<radialGradient>を入れる
+		this.elmGrp = this.newTagSVG("g"); //<circle>を入れる
 		const clip = this.newTagSVG("clipPath");
-		clip.setAttribute("clip-rule","evenodd");
-		clip.appendChild(
-			//<path>を生成して直接append
-			( p =>{
-				p.setAttribute("d",dstr);
-				return p;
-			})( this.newTagSVG("path") )
-		);
-		//<defs> HTML化した時に(多少)わかりやすくなる(はず)
-		const defz = this.newTagSVG("defs");
-		defz.appendChild(clip);
-		//<g>
-		const grp = this.newTagSVG("g");
-		grp.id = "thatring";
-		clip.id = "path"+grp.id; //<g>のidを加工して<clipPath>のidを決める(ことで重複させない)
-		grp.setAttribute("clip-path",`url(#${clip.id})`);
+		this.elmPath = this.newTagSVG("path");
 
-		//「色配置」円形に配置
-		for( let index = 0 ; index < colorinstr.length ; ++index ){
-			//グラデーション設定
-			let raGdt = this.newTagSVG("radialGradient");
-			raGdt.id = `filler${index}`;
-			let s1 = this.newTagSVG("stop");
-			s1.setAttribute("offset","0");
-			s1.setAttribute("stop-color",colorinstr[index]);
-			s1.setAttribute("stop-opacity","1");
-			let s2 = this.newTagSVG("stop");
-			s2.setAttribute("offset","1");
-			s2.setAttribute("stop-color",colorinstr[index]);
-			s2.setAttribute("stop-opacity","0");
-			raGdt.appendChild(s1);
-			raGdt.appendChild(s2);
-			defz.appendChild(raGdt);
-			//上を利用した円の描画
-			let c1 = this.newTagSVG("circle");
-			c1.cx.baseVal.value = rad_midstrip*Math.sin(2*Math.PI*index/colorinstr.length);
-			c1.cy.baseVal.value = rad_midstrip*Math.cos(2*Math.PI*index/colorinstr.length);
-			c1.r.baseVal.value = gap_colorcircle;
-			c1.style.fill = `url(#${raGdt.id})`;
-			grp.appendChild(c1);
-		}
+		svgroot.appendChild(this.elmDefs);
+		svgroot.appendChild(this.elmGrp);
+		this.elmDefs.appendChild(clip);
+		clip.appendChild( this.elmPath );
+
+		svgroot.setAttribute("viewBox", "-1 -1 2 2");
+		clip.setAttribute("clip-rule","evenodd");
+
+		//クリッピング設定をつなぎ合わせる
+		clip.id = "thatpath";
+		this.elmGrp.setAttribute("clip-path",`url(#${clip.id})`);
 
 		//<style> 専用 CSS Style
 		let style = document.createElement("style");
@@ -151,15 +87,158 @@ class imascgssThatring extends HTMLElement{
 			width: var(--length-size,50vmin);
 		}`;
 
-		//<svg> SVGを構成・接続する
-		const rootsvg = this.newTagSVG("svg");
-		rootsvg.setAttribute("viewBox", "-1 -1 2 2");
-		rootsvg.appendChild(defz);
-		rootsvg.appendChild(grp);
-		//Shadow Root を作って、そこにぶら下げる
+		//Shadow Root を作って、そこにぶら下げる。これでshadow DOM tree は完成
 		let shdwroot = this.attachShadow({mode:"open"})
 		shdwroot.appendChild(style);
-		shdwroot.appendChild(rootsvg);
+		shdwroot.appendChild(svgroot);
+
+		//初期設定値を適用 : radiusMidstrip はここで最初に設定
+		this.radiusMidstrip = this.calcMidstrip( this.normalize( this.integral(
+			(a=>{
+				//二重配列を単配列にする；頭2要素のみ
+				let b=[],c;
+				for(c of a){ b.push(c[0],c[1]) }
+				return b;
+			})(imascgssThatring.defaultStripJson)
+		))[0]);
+		this.updateColors(imascgssThatring.defaultColors);
+		this.updateClipPath(imascgssThatring.defaultStripJson);
 	}
+
+
+	//グラデーションと円の個数連携(新しい円の数)
+	updateGradientCirclesRelation(_newLength){
+		let aGradients = this.getTagsSVG("radialGradient",this.elmDefs);
+		let aCircles = this.getTagsSVG("circle",this.elmGrp);
+		//多い分減らす
+		while(_newLength < aCircles.length){
+			aGradients[aGradients.length-1].remove();
+			aCircles[aCircles.length-1].remove();
+		}
+		//少ない分増やす
+		while(aCircles.length < _newLength){
+			let nxtidx = aCircles.length;
+			//グラデーション設定
+			let raGdt = this.newTagSVG("radialGradient");
+			raGdt.id = `filler${nxtidx}`;
+			let s1 = this.newTagSVG("stop");
+			s1.setAttribute("offset","0");
+			s1.setAttribute("stop-opacity","1");
+			let s2 = this.newTagSVG("stop");
+			s2.setAttribute("offset","1");
+			s2.setAttribute("stop-opacity","0");
+			raGdt.appendChild(s1);
+			raGdt.appendChild(s2);
+			this.elmDefs.appendChild(raGdt);
+			//上を利用して円を描画
+			let c1 = this.newTagSVG("circle");
+			c1.style.fill = `url(#${raGdt.id})`;
+			this.elmGrp.appendChild(c1);
+		}
+	}
+
+	//グラデーション円の位置調整
+	updateGradientCirclesPos(){
+		//circleのcollectionを準備
+		let aCircles = this.getTagsSVG("circle",this.elmGrp);
+		//色付け円の中心同士の距離(＝指定する半径)
+		let gapCircle = this.radiusMidstrip * this.edgelen_regpoly(aCircles.length);
+		for( let i=0; i<aCircles.length; ++i){
+			let theta = 2*Math.PI*i/aCircles.length
+			aCircles[i].cx.baseVal.value = this.radiusMidstrip*Math.sin(theta);
+			aCircles[i].cy.baseVal.value = this.radiusMidstrip*Math.cos(theta);
+			aCircles[i].r.baseVal.value = gapCircle;
+		}
+	}
+
+	//色配置の更新
+	updateColors(_aData){
+		//radialGradientの数と新しいデータの数を比較して
+		let aGradients = this.getTagsSVG("radialGradient",this.elmDefs);
+		if( aGradients.length !== _aData.length){
+			//1. 等しくないなら、radialGradient と circle の過不足を調整して、更にcircle の位置を調整
+			this.updateGradientCirclesRelation(_aData.length);
+			this.updateGradientCirclesPos();
+		}
+		//2. radialGradient > stop-color 更新
+		//aGradientsは「生きている」ので、個数が変更されていてもへっちゃら。
+		for( let i = 0 ; i < _aData.length ; ++i ){
+			aGradients[i].firstElementChild.setAttribute("stop-color",_aData[i]);
+			aGradients[i].lastElementChild.setAttribute("stop-color",_aData[i]);
+		}
+	}
+
+	//クリップパスの更新
+	updateClipPath(_aData){
+		//入力データをpath/dに変換しやすい状態に加工する
+		let aRanges=[], aaHints=[]; //半径配列とヒント二重配列
+		while(_aData.length>0){
+			let a = _aData.shift();
+			if( Array.isArray(a) ){
+				//期待する配列は、[数,数,配列,配列]
+				aRanges.push( this.toIntZeroPlus(a[0]), this.toIntZeroPlus(a[1]) );
+				aaHints.push( this.toArrayIntZeroPlus(a[2]), this.toArrayIntZeroPlus(a[3]) );
+			}else{
+				console.warn(`(updateClipPath) ${a} is skipped; ain't Array.`);
+			}
+		}
+		//aRangesは積分して0～1に標準化、aaHintsの各要素は積分して0～2piに標準化する。
+		aRanges = this.normalize( this.integral(aRanges) );
+		aaHints = aaHints.map( a=>
+			a.length>0 ? this.normalize( this.integral(a), 2*Math.PI ) : a
+		);
+		//円環の中央部半径
+		this.radiusMidstrip = this.calcMidstrip(aRanges[0]);
+		this.updateGradientCirclesPos(); //radiusMidstripが変更されたので
+
+		//変換してpath/dに渡す
+		let i=0, strPathD="";
+		for( ; i < aRanges.length ; ++i ){
+			let r = aRanges[i]; //対象半径
+			strPathD += `M${r},0`;
+			if( aaHints[i].length === 0 ){
+				strPathD += `A${r},${r} 0 0 ${i&1} -${r},0`;
+				strPathD += `A${r},${r} 0 0 ${i&1} ${r},0z`;
+			}
+			else {
+				let q = i<1 ? 0 : aRanges[i-1]; //直前半径
+				let ah = aaHints[i]; //配列：切り替える角度
+				for( let j = 0 ; j < ah.length ; ++j ){
+					let s = (j&1?q:r), t = (j&1?r:q); //s=主、t=従 //反時計回りはY方向を反転
+					let mx = Math.cos(ah[j]), my = Math.sin(i&1?ah[j]:-ah[j]); //
+					strPathD += `A${s},${s} 0 0 ${i&1} ${s*mx},${s*my}`; //円弧
+					strPathD += (j+1 === ah.length ? "z" : `L${t*mx},${t*my}`); //直線
+				}
+			}
+		}	// A rx,ry 楕円の傾き 大回り(1|0) 時計回り(1|0) x,y
+		this.elmPath.setAttribute("d",strPathD);
+	}
+
+	//<callback>属性に変更があった
+	attributeChangedCallback(_attrName, _oldVal, _newVal){
+		console.log(`aCCb : ${_attrName}`);
+		let newData;
+		switch(_attrName){
+			case "data-colors":
+				newData = this.csv2arrStr(_newVal);
+				if( newData.length<=5 ){
+					console.warn(`data-colors="${_newVal}" 6色以上指定してください。初期値を使用します`);
+					newData = imascgssThatring.defaultColors;
+				}
+				this.updateColors(newData);
+				break;
+			case "data-json":
+				try{
+					newData = JSON.parse(_newVal);
+				}catch(e){
+					console.warn(`data-json="${_newVal}" 変換失敗。初期値を使用します`);
+					newData = imascgssThatring.defaultStripJson;
+				}
+				this.updateClipPath(newData);
+				break;
+		}
+	}
+	static get observedAttributes(){ return ["data-colors","data-json"] }
+
 } //end_of_class imascgssThatring
 window.customElements.define('imascgss-thatring',imascgssThatring);
